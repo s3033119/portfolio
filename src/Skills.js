e6f88d67cfe8a28a90b7815d88fc9c6406d96177
日高智大 <s3033119@yahoo.co.jp>
2020-08-02 16:54:14 +0900

    Fixed some bugs and finished product.

--- src/Skills.js
+++ src/Skills.js
@@ -16,16 +16,22 @@ export default class Home extends React.Component {
             <div className="skill-group">
               {/* シンプル表示 */}
               { this.props.isSimple && (
-                  <ul>
+                  <table>
+                    <tr className="simple-skill-list">
+                      <th>スキル名</th>
+                      <th>習熟度</th>
+                      <th>詳細</th>
+                    </tr>
                     {this.props.skills.map((skill)=> {
                         return(
-                            <li>
-                              <span>{skill.name}</span>
-                              <Stars rate={skill.rate}/>
-                            </li>
+                            <tr className="simple-skill-list">
+                              <td className="simple-skill-name">{skill.name}</td>
+                              <td><Stars rate={skill.rate}/></td>
+                              <td className="simple-skill-description">{skill.description}</td>
+                            </tr>
                         );
                     })}
-                  </ul>
+                  </table>
               )}
 
               {/* サムネイル表示 */}
@@ -36,12 +42,13 @@ export default class Home extends React.Component {
 
                             <div className="skill-content"
                                  style={{
-                                     'box-shadow': '1px 0 5px 1px ' + this.props.color,
-                                 }} data-tip={skill.description}>
+                                     'boxShadow': '1px 0 5px 1px ' + this.props.color,
+                                 }}>
 
-                              <ReactTooltip effect="solid" type="light" textColor="black" place="bottom"/>
-                              <div className="skill-icon">
+                              <div className="skill-icon" data-tip={skill.description}>
                                 <img src={require("./icons/"+skill.image)}/>
+                                <ReactTooltip className="react-tooltip" effect="solid" type="light" place="bottom" backgroundColor="rgba(0,0,0,.7)" textColor="white"/>
+
                               </div>
                               <span className="skill-name">{skill.name}</span>
                               <Stars rate={skill.rate}/>
